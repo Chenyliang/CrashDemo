2b35d930d003b77e63e443234c7d76317472ab59
Chenyliang <603440374@qq.com>
2024-10-14 09:28:09 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CrashDemo
+
 Memory Leak and Crash In SciChart 3D 
+
+The situation occurs in two configurations.
+
+1、 i7-9700 + Graphics card: Intel(R) UHD Graphics 630 
+
+2、i7-12700 + RTX 3050
---
+++ SciChart3DCrash/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SciChart3DCrash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var window = new SciChart3DWindow();
+                window.Show();
+                await Task.Delay(2000);
+                window.Close();
+            }
+        }
+
+        private async void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var window = new SciChart2DWindow();
+                window.Show();
+                await Task.Delay(2000);
+                window.Close();
+            }
+        }
+    }
+}
