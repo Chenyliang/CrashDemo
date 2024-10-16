0e228dd87cc33ac007886bd3af42d2d85ee435c0
Chenyliang <49751926+Chenyliang@users.noreply.github.com>
2024-10-16 10:10:21 +0800

    Update App.xaml.cs

--- SciChart3DCrash/App.xaml.cs
+++ SciChart3DCrash/App.xaml.cs
@@ -12,7 +12,7 @@ namespace SciChart3DCrash
     {
         public App()
         {
-            SciChartSurface.SetRuntimeLicenseKey("+9oEmXJwJCPhHXyBawd59wjhOdu9XRwFwJQGaoUjRnPa52M7nY7ysYeo/OYo1ibNL1PNBRODaA+eckDN8bcENzfQSVJwrdqLl9uinnYrpXZVgABp8pFV5JqM1zvtQqjRn7U6yDp6/fUtR9VzD58DJRbdc73cVlCKxtFT0zYLbbdCScUrON4EC6Pci2+je2Rp5dJ8kTzKvO+o3+WPIZoCB+mq6WWTjY/XKGF82Ph1WyI3cLpZEqvlQPITFIpLiG40nzFUbXT9PYkn0+JWKBxJ3q8e0Lg6TZE5cWMFosvm96WlQS9IazpSDy2pJV5+7pPC+IbDRt5HYVFmjoD7pa7WSUfFjTOqiA0Nk+1gIqg9qWxy/NRcX/yxffbb4hcqU+KKmC1zORfJim0JH8x0zNZEx7KrsKK3gwnG+Ftw53FUJf5CDAoU4btl2MXQBeaYTy1WOzpERklsCyPFxuJq5ljdUSE8qWiVGvXJcWJEZQxSfGEnM54Lq/ax3/LPymM0o8kL/t4E+oV97OQZyxC2fFF87DfjL/9rf48bvYHz7A=="); 
+            SciChartSurface.SetRuntimeLicenseKey("your code"); 
 
 
         }
